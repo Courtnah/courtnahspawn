0bfe60b5b6491f56a5a246911afb248b09f4080d
courtnah <courtnah@courtnah-Inspiron-7773>
2022-05-16 21:51:14 -0400

    top ring

--- pawn.groovy
+++ pawn.groovy
@@ -1,13 +1,21 @@
 //Your code here
+def height= 40
 CSG myCylinder = new Cylinder(20, // Radius at the bottom
                       		10, // Radius at the top
-                      		40, // Height
-                      		(int)30 //resolution
+                      		height, // Height
+                      		(int)60 //resolution
                       		).toCSG()//convert to CSG to dispr
 
 CSG sphereHighRes = new Sphere(20,// Spheres radius
 						40,//elevation Divisions
 						40)// vertical divisions
 						.toCSG()// convert to CSG to display
-						.movez(55)
-return [myCylinder, sphereHighRes]
+						.movez(height)
+CSG roundedCylinder = new RoundedCylinder(40,15)
+                                .cornerRadius(10)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+CSG UpperRoundedCylinder = new RoundedCylinder(40,15.0)
+                                .cornerRadius(10)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+
+return [myCylinder, sphereHighRes, roundedCylinder, UpperRoundedCylinder]
