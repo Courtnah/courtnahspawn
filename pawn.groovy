8bbdf1a4f78d29f1d547dd6ed3a598483d8441cf
courtnah <courtnah@courtnah-Inspiron-7773>
2022-05-16 23:29:38 -0400

    round 1 complete with Hull and Union

--- pawn.groovy
+++ pawn.groovy
@@ -2,7 +2,7 @@
 def height= 50
 CSG myCylinder = new Cylinder(20, // Radius at the bottom
                       		10, // Radius at the top
-                      		height, // Height
+                      		height+2, // Height
                       		(int)60 //resolution
                       		).toCSG()//convert to CSG to dispr
 
@@ -14,16 +14,20 @@ CSG sphereHighRes = new Sphere(15,// Spheres radius
 CSG TwoRoundedCylinder = new RoundedCylinder(30,10)
                                 .cornerRadius(5)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
+                               
 CSG UpperRoundedCylinder = new RoundedCylinder(15,10.0)
                                 .cornerRadius(7)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
                                 .movez(height)
-CSG OneRoundedCylinder = new RoundedCylinder(17,10)
-                                .cornerRadius(7)
+CSG OneRoundedCylinder = new RoundedCylinder(7,10)
+                                .cornerRadius(10)
                                 .toCSG()// converts it to a CSG tor display
-                                .movez(5)
-                                .movex(5)
+                                .movez(10)
+                                
 
 CSG Bottom= OneRoundedCylinder.union(TwoRoundedCylinder).hull();
 
-return [myCylinder, sphereHighRes, UpperRoundedCylinder, Bottom]
+
+CSG allUnion = CSG.unionAll([myCylinder, sphereHighRes, UpperRoundedCylinder, Bottom])
+
+return (allUnion)
