8f19b304dd76b143604a1abeb11c9061adcd3769
courtnah <courtnah@courtnah-Inspiron-7773>
2022-05-16 21:27:55 -0400

    established cylinder

--- pawn.groovy
+++ pawn.groovy
@@ -1,1 +1,7 @@
 //Your code here
+CSG myCylinder = new Cylinder(20, // Radius at the bottom
+                      		10, // Radius at the top
+                      		40, // Height
+                      		(int)30 //resolution
+                      		).toCSG()//convert to CSG to dispr
+return [myCylinder]
