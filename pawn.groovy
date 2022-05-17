8f2f339708d0eae39f9aa3441feb571fcefbd7ba
courtnah <courtnah@courtnah-Inspiron-7773>
2022-05-16 22:09:43 -0400

    Ignore the project files

--- pawn.groovy
+++ pawn.groovy
@@ -6,17 +6,20 @@ CSG myCylinder = new Cylinder(20, // Radius at the bottom
                       		(int)60 //resolution
                       		).toCSG()//convert to CSG to dispr
 
-CSG sphereHighRes = new Sphere(18,// Spheres radius
+CSG sphereHighRes = new Sphere(15,// Spheres radius
 						40,//elevation Divisions
 						40)// vertical divisions
 						.toCSG()// convert to CSG to display
-						.movez(height+35)
-CSG roundedCylinder = new RoundedCylinder(40,15)
+						.movez(height+30)
+CSG TwoRoundedCylinder = new RoundedCylinder(20,15)
                                 .cornerRadius(10)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
                                 .movez(height)
-CSG UpperRoundedCylinder = new RoundedCylinder(40,15.0)
-                                .cornerRadius(10)// sets the radius of the corner
+CSG UpperRoundedCylinder = new RoundedCylinder(30,15.0)
+                                .cornerRadius(5)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+CSG OneRoundedCylinder = new RoundedCylinder(20,15.0)
+                                .cornerRadius(5)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
 
-return [myCylinder, sphereHighRes, roundedCylinder, UpperRoundedCylinder]
+return [myCylinder, sphereHighRes, UpperRoundedCylinder, TwoRoundedCylinder, OneRoundedCylinder]
