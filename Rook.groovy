21e2ff764f72b35aedc3cab88a1cbac0a5196b4d
courtnah <courtnah@courtnah-Inspiron-7773>
2022-05-16 23:47:30 -0400

    Returning the return

--- Rook.groovy
+++ Rook.groovy
@@ -1,1 +1,28 @@
-//Your code here
+
+def height= 50
+CSG myCylinder = new Cylinder(20, // Radius at the bottom
+                      		10, // Radius at the top
+                      		height+2, // Height
+                      		(int)60 //resolution
+                      		).toCSG()//convert to CSG to dispr
+
+CSG sphereHighRes = new Sphere(15,// Spheres radius
+						40,//elevation Divisions
+						40)// vertical divisions
+						.toCSG()// convert to CSG to display
+						.movez(height+20)
+CSG TwoRoundedCylinder = new RoundedCylinder(30,10)
+                                .cornerRadius(5)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+                               
+CSG UpperRoundedCylinder = new RoundedCylinder(15,10.0)
+                                .cornerRadius(7)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+                                .movez(height)
+CSG OneRoundedCylinder = new RoundedCylinder(7,10)
+                                .cornerRadius(10)
+                                .toCSG()// converts it to a CSG tor display
+                                .movez(10)
+CSG Bottom= OneRoundedCylinder.union(TwoRoundedCylinder).hull();
+
+return (myCylinder,sphereHighRes,UpperRoundedCylinder,Bottom)
