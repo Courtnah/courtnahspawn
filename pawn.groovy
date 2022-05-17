c8e9d1cfae1a6c71e85665a9240f21b38633a875
courtnah <courtnah@courtnah-Inspiron-7773>
2022-05-16 22:48:46 -0400

    cylinder detailing

--- pawn.groovy
+++ pawn.groovy
@@ -1,5 +1,5 @@
 //Your code here
-def height= 40
+def height= 50
 CSG myCylinder = new Cylinder(20, // Radius at the bottom
                       		10, // Radius at the top
                       		height, // Height
@@ -10,16 +10,17 @@ CSG sphereHighRes = new Sphere(15,// Spheres radius
 						40,//elevation Divisions
 						40)// vertical divisions
 						.toCSG()// convert to CSG to display
-						.movez(height+30)
-CSG TwoRoundedCylinder = new RoundedCylinder(20,15)
-                                .cornerRadius(10)// sets the radius of the corner
+						.movez(height+20)
+CSG TwoRoundedCylinder = new RoundedCylinder(30,15)
+                                .cornerRadius(5)// sets the radius of the corner
+                                .toCSG()// converts it to a CSG tor display
+CSG UpperRoundedCylinder = new RoundedCylinder(15,10.0)
+                                .cornerRadius(7)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
                                 .movez(height)
-CSG UpperRoundedCylinder = new RoundedCylinder(30,15.0)
-                                .cornerRadius(5)// sets the radius of the corner
+CSG OneRoundedCylinder = new RoundedCylinder(25,10)
+                                .cornerRadius(8)// sets the radius of the corner
                                 .toCSG()// converts it to a CSG tor display
-CSG OneRoundedCylinder = new RoundedCylinder(20,15.0)
-                                .cornerRadius(5)// sets the radius of the corner
-                                .toCSG()// converts it to a CSG tor display
+                                .movez(15)
 
 return [myCylinder, sphereHighRes, UpperRoundedCylinder, TwoRoundedCylinder, OneRoundedCylinder]
